3c452430a6a54d52f0246fdc1aa8a529cc484d54
Adriano Shineyder <adrianoshineyder@hotmail.com>
2022-05-24 22:19:51 -0300

    Atualização nos endpoints

--- routes/api.php
+++ routes/api.php
@@ -30,10 +30,11 @@ Route::prefix('user')->middleware('apiJWT')->group(function () {
     Route::get('/{id}', [UserController::class, 'info'])->name('user.info');
     Route::put('/{id}', [UserController::class, 'update'])->name('user.update');
     Route::delete('/{id}', [UserController::class, 'delete'])->name('user.delete');
+    Route::post('/permission', [UserPermissionController::class, 'updateMultiplePermission'])->name('permission.update');
 });
 
 /* Route::middleware('apiJWT')->post('/permission', [UserPermissionController::class, 'updateOnePermission'])->name('permission.update'); */
 
-Route::middleware('apiJWT')->post('/permission', [UserPermissionController::class, 'updateMultiplePermission'])->name('permission.update');
+
 
 Route::middleware('apiJWT')->get('/resource', [ResourceController::class, 'list'])->name('permission.list');
